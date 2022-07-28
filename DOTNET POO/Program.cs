0658f5e68ebcadbdb0ba53e006b45ae8530194e2
Fabio  Santos <fabioov@hotmail.com>
2022-07-28 15:22:05 -0300

    add new repo and updating files

---
+++ DOTNET POO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+using System;
+using DOTNET_POO.src.Entities;
+
+
+namespace DOTNET_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero("Arus", 23, "Knight");
+            Hero wizard = new Hero("Jennica", 23, "Wizard");
+
+
+
+            System.Console.WriteLine(arus.Attack());
+            System.Console.WriteLine(wizard.Attack());
+        }
+    }
+}
--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,37 +1,30 @@
-import { NgModule, Pipe } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { Error404Component } from './error-404/error-404.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './star/star.component';
+
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent, 
-    ReplacePipe, 
-    NavBarComponent,
-    Error404Component
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
+
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: '**', component: Error404Component
       }
+
     ])
   ],
   providers: [],
